67f29635e1aa7e35c52acf2091a2752722bc5249
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-06-18 15:54:54 +0300

    Trying to get something visible in UI

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('RedditClientApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, geolocation) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    geolocation.getCurrentPosition(function(position) {
+      $scope.position = position;
+    });
   });
--- app/scripts/services/geolocation.js
+++ app/scripts/services/geolocation.js
@@ -1,3 +1,4 @@
+// jshint unused: false
 'use strict';
 
 angular.module('RedditClientApp')
