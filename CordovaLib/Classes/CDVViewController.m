296bc8c2115189e6d094c5c6f058d6d752dcdbc9
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-06-18 15:10:11 +0300

    Change Cordova wwwFolderName to be "app"
    This should load the Angular app.

--- CordovaLib/Classes/CDVViewController.m
+++ CordovaLib/Classes/CDVViewController.m
@@ -208,7 +208,7 @@
     self.settings = delegate.settings;
 
     // And the start folder/page.
-    self.wwwFolderName = @"www";
+    self.wwwFolderName = @"app";
     self.startPage = delegate.startPage;
     if (self.startPage == nil) {
         self.startPage = @"index.html";
